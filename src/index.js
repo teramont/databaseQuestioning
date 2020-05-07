4c28c70dbcd66ed2e9eb701a4ce2631ca62106cc
Alexander3006 <ukrpresident2036@gmail.com>
2020-05-07 15:49:10 +0300

    some fix, catch err

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ const db = require('./db');
 
 const exapmleQuery = async (idUser) => {
   const sql = `SELECT * FROM user WHERE idUser = ?`;
-  const data = await db.query(sql, [idUser]);
-  return data[0];
+  try {
+    const data = await db.query(sql, [idUser]);
+    return data[0];
+  } catch (err) {
+    console.dir(err);
+  }
+  return;
 };
+
 (async () => {
   const data = await exapmleQuery(1);
   console.dir(data);
