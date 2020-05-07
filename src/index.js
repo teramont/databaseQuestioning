c42743fe7859208935254a68940cbac1da73a704
Alexander3006 <ukrpresident2036@gmail.com>
2020-05-07 15:41:59 +0300

    database connection, mysql2

---
+++ src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('dotenv')
+    .config();
+
+const db = require('./db');
+
+const exapmleQuery = async (idUser) => {
+  const sql = `SELECT * FROM user WHERE idUser = ?`;
+  const data = await db.query(sql, [idUser]);
+  return data[0];
+};
+(async () => {
+  const data = await exapmleQuery(1);
+  console.dir(data);
+  await db.end();
+})();
+
