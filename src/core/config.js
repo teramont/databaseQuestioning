43101e82ba0dfdd05f759648c8e99bd7fd3a34d5
RomaShpylovyi <44481626+ShpylovyiRoman@users.noreply.github.com>
2020-05-13 09:45:45 +0300

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -2,15 +2,12 @@
 
 const {createPool} = require('mysql2');
 
-const {DB_HOST, DB_POOL_SIZE,
-  DB_USER, DB_NAME, DB_PASS} = process.env;
-
-const db = createPool({
-  connectionLimit: DB_POOL_SIZE,
-  host: DB_HOST,
-  user: DB_USER,
-  database: DB_NAME,
-  password: DB_PASS,
-}).promise();
+  const db = createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',
+    database: 'survey',
+    password: '30062001',
+  }).promise();
 
 module.exports = db;
